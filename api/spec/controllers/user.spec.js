49c2b479b820f828c47fe7915d04085996556533
Abigail <ajmakovsky@gmail.com>
2023-03-28 17:56:29 +0100

    Backend Controller structure update

--- api/spec/controllers/user.spec.js
+++ api/spec/controllers/user.spec.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const request = require("supertest");
 const app = require("../../app");
@@ -193,6 +194,77 @@ describe("/users", () => {
           expect(users.length).toEqual(0);
         });
       });
+    });
+  });
+
+  describe("/user", () => {
+    // beforeAll(async () => {
+    //   const user = await new User({
+    //     firstName: "User",
+    //     lastName: "One",
+    //     email: "test@test.com",
+    //     password: bcrypt.hashSync("12345678", bcrypt.genSaltSync()),
+    //   });
+    //   await user.save();
+    // });
+
+    // afterAll(async () => {
+    //   await User.deleteMany({});
+    // });
+
+    test("a token is returned when login credentials are valid", async () => {
+      const user = await new User({
+        firstName: "User",
+        lastName: "One",
+        email: "test@test.com",
+        password: bcrypt.hashSync("12345678", bcrypt.genSaltSync()),
+      });
+      await user.save();
+
+      const response = await request(app)
+        .post("/user/login")
+        .send({ email: "test@test.com", password: "12345678" });
+      expect(response.status).toEqual(201);
+      expect(response.body.token).not.toEqual(undefined);
+      expect(response.body.message).toEqual("Login Successful");
+    });
+
+    test("a token is not returned when password is invalid", async () => {
+      const user = await new User({
+        firstName: "User",
+        lastName: "One",
+        email: "test@test.com",
+        password: bcrypt.hashSync("12345678", bcrypt.genSaltSync()),
+      });
+      await user.save();
+
+      const response = await request(app)
+        .post("/user/login")
+        .send({ email: "test@test.com", password: "1234" });
+      expect(response.status).toEqual(401);
+      expect(response.body.token).toEqual(undefined);
+      expect(response.body.message).toEqual(
+        "Username or password is incorrect"
+      );
+    });
+
+    test("a token is not returned when email is invalid", async () => {
+      const user = await new User({
+        firstName: "User",
+        lastName: "One",
+        email: "test@test.com",
+        password: bcrypt.hashSync("12345678", bcrypt.genSaltSync()),
+      });
+      await user.save();
+
+      const response = await request(app)
+        .post("/user/login")
+        .send({ email: "test1@test.com", password: "12345678" });
+      expect(response.status).toEqual(401);
+      expect(response.body.token).toEqual(undefined);
+      expect(response.body.message).toEqual(
+        "Username or password is incorrect"
+      );
     });
   });
 });
