55c76ab85e7bea11f2b31d8b78bfe4adf8a376f6
Joel Powell <joelhpowell@icloud.com>
2023-03-29 22:49:06 +0100

    create results page

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -3,7 +3,7 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "../home/Home";
 import Signup from "../signup/Signup";
 import LoginForm from "../Login/loginForm";
-
+import Results from "../results/Results";
 
 const App = () => {
   return (
@@ -12,7 +12,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<LoginForm />} />
-
+        <Route path="/results" element={<Results />} />
       </Routes>
     </BrowserRouter>
   );
