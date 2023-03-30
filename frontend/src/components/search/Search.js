2c21a877e59de871ffe90bfc3ca9671ca177be23
Joel Powell <joelhpowell@icloud.com>
2023-03-30 08:14:26 +0100

    add loading and empty state for search results

--- frontend/src/components/search/Search.js
+++ frontend/src/components/search/Search.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Combobox } from "@headlessui/react";
+import { Combobox, Transition } from "@headlessui/react";
 import useGoogle from "react-google-autocomplete/lib/usePlacesAutocompleteService";
 import clsx from "clsx";
 import { Link } from "react-router-dom";
@@ -7,14 +7,15 @@ import { Link } from "react-router-dom";
 const Search = () => {
   const [selectedPlace, setSelectedPlace] = useState(null);
 
-  const { placePredictions, getPlacePredictions } = useGoogle({
-    apiKey: process.env.REACT_APP_API_KEY,
-    debounce: 500,
-    options: {
-      types: ["address"],
-      componentRestrictions: { country: "gb" },
-    },
-  });
+  const { placePredictions, getPlacePredictions, isPlacePredictionsLoading } =
+    useGoogle({
+      apiKey: process.env.REACT_APP_API_KEY,
+      debounce: 500,
+      options: {
+        types: ["address"],
+        componentRestrictions: { country: "gb" },
+      },
+    });
 
   return (
     <>
@@ -37,45 +38,62 @@ const Search = () => {
           placeholder="Search by address"
         />
 
-        {placePredictions.length > 0 && (
+        <Transition
+          enter="transition duration-100 ease-out"
+          enterFrom="transform scale-95 opacity-0"
+          enterTo="transform scale-100 opacity-100"
+          leave="transition duration-75 ease-out"
+          leaveFrom="transform scale-100 opacity-100"
+          leaveTo="transform scale-95 opacity-0"
+        >
           <Combobox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-2xl bg-white/5 text-white shadow-lg ring-1 ring-white/10 backdrop-blur-md focus:outline-none sm:text-sm">
-            {placePredictions.map(
-              ({
-                description,
-                place_id: id,
-                structured_formatting: {
-                  main_text: mainText,
-                  secondary_text: secondaryText,
-                },
-              }) => (
-                <Combobox.Option
-                  key={id}
-                  value={{ description, id }}
-                  className={({ active }) =>
-                    clsx(
-                      "relative cursor-default select-none py-2 pl-3 pr-9 text-white",
-                      active && "bg-orange-500"
-                    )
-                  }
-                >
-                  {({ active }) => (
-                    <div className="flex">
-                      <span className="truncate">{mainText}</span>
-                      <span
-                        className={clsx(
-                          "ml-2 truncate text-white/60",
-                          active && "text-orange-200"
-                        )}
-                      >
-                        {secondaryText}
-                      </span>
-                    </div>
-                  )}
-                </Combobox.Option>
+            {isPlacePredictionsLoading ? (
+              <div className="relative animate-pulse cursor-default select-none py-2 text-white">
+                Loading...
+              </div>
+            ) : placePredictions.length === 0 ? (
+              <div className="relative cursor-default select-none py-2 text-white">
+                No properties found
+              </div>
+            ) : (
+              placePredictions.map(
+                ({
+                  description,
+                  place_id: id,
+                  structured_formatting: {
+                    main_text: mainText,
+                    secondary_text: secondaryText,
+                  },
+                }) => (
+                  <Combobox.Option
+                    key={id}
+                    value={{ description, id }}
+                    className={({ active }) =>
+                      clsx(
+                        "relative cursor-default select-none py-2 pl-3 pr-9 text-white",
+                        active && "bg-orange-500"
+                      )
+                    }
+                  >
+                    {({ active }) => (
+                      <div className="flex">
+                        <span className="truncate">{mainText}</span>
+                        <span
+                          className={clsx(
+                            "ml-2 truncate text-white/60",
+                            active && "text-orange-200"
+                          )}
+                        >
+                          {secondaryText}
+                        </span>
+                      </div>
+                    )}
+                  </Combobox.Option>
+                )
               )
             )}
           </Combobox.Options>
-        )}
+        </Transition>
       </Combobox>
       <div className="absolute inset-y-0 right-0 flex p-1.5">
         <Link
