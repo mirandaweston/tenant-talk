157a27f1a54991e350af6d35d800657f99bc82ab
Joel Powell <joelhpowell@icloud.com>
2023-03-30 11:22:17 +0100

    add use of url seearch params

--- frontend/src/components/results/Results.js
+++ frontend/src/components/results/Results.js
@@ -1,13 +1,31 @@
-import { useLocation } from "react-router-dom";
 import React from "react";
+import { useSearchParams } from "react-router-dom";
+import useAxios from "axios-hooks";
+import useAuthContext from "../../hooks/useAuthContext";
 
 const Results = () => {
-  const location = useLocation();
+  const [searchParams] = useSearchParams();
+  const { token } = useAuthContext();
+
+  const [{ loading, data, error }] = useAxios({
+    url: "/property/address",
+    method: "GET",
+    headers: { Authorization: `Bearer ${token}` },
+    params: {
+      address: searchParams.get("description"),
+    },
+  });
 
   return (
     <>
-      <div>You searched for:</div>
-      {location.state.description && <div>{location.state.description}</div>}
+      <div>Hello</div>
+      <div>Search results:</div>
+
+      {error && <div>{error.message}</div>}
+      {loading && <div>Currently loading</div>}
+
+      {data && <div>{data.property.address}</div>}
+      {data && <div>{data.property._id}</div>}
     </>
   );
 };
