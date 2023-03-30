fd22a61e9518ecc6b6d8b8fb9ec99f90fd036948
Hayley Dobbs <iamdobbs@icloud.com>
2023-03-31 00:35:32 +0100

    Add ReviewCard component and separate styling func from reviewsList.js

--- frontend/src/components/reviewCard/reviewCard.js
+++ frontend/src/components/reviewCard/reviewCard.js
@@ -1,126 +1,44 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { Link } from "react-router-dom";
+import React from "react";
 import { StarIcon } from "@heroicons/react/20/solid";
-import useAuthContext from "../../hooks/useAuthContext";
+import clsx from "clsx";
+import { format } from 'date-fns';
 
-const useReviewCard = () => {
-  const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(null);
-  const { dispatch } = useAuthContext();
+// 
 
-  const fetchReview = async () => {
-    setIsLoading(true);
-    setError(null);
+function ReviewCard(props) {
+  const { review } = props;
 
-    try {
-      const { data } = await axios.get(`/property/ '${propertyId}`)
-    }
-  }
-}
-/*
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/typography'),
-    ],
-  }
-  ```
-*/
-
-const reviews = [
-  {
-    id: 1,
-    rating: 5,
-    content: `
-      <p>This icon pack is just what I need for my latest project. There's an icon for just about anything I could ever need. Love the playful look!</p>
-    `,
-    date: "July 16, 2021",
-    datetime: "2021-07-16",
-    author: "Emily Selman",
-    avatarSrc:
-      "https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80",
-  },
-  {
-    id: 2,
-    rating: 5,
-    content: `
-      <p>Blown away by how polished this icon pack is. Everything looks so consistent and each SVG is optimized out of the box so I can use it directly with confidence. It would take me several hours to create a single icon this good, so it's a steal at this price.</p>
-    `,
-    date: "July 12, 2021",
-    datetime: "2021-07-12",
-    author: "Hector Gibbons",
-    avatarSrc:
-      "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80",
-  },
-  // More reviews...
-];
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
-}
-
-const ReviewCard = () => {
   return (
-    <div className="bg-white">
-      <div>
-        <h2 className="sr-only">Customer Reviews</h2>
+    <li className="overflow-hidden rounded-md bg-white px-6 py-4 shadow">
+      <div className="flex space-x-4 text-sm text-gray-500">
+        <div className="flex-1 py-10">
+          <h3 className="font-medium text-gray-900">{review.author.firstName}</h3>
+          <p>
+            <time dateTime={review.date}>{format(new Date(review.createdAt), 'dd, MMMM, yyyy')}</time>
+          </p>
 
-        <div className="-my-10">
-          {reviews.map((review, reviewIdx) => (
-            <div
-              key={review.id}
-              className="flex space-x-4 text-sm text-gray-500"
-            >
-              <div className="flex-none py-10">
-                <img
-                  src={review.avatarSrc}
-                  alt=""
-                  className="h-10 w-10 rounded-full bg-gray-100"
-                />
-              </div>
-              <div
-                className={classNames(
-                  reviewIdx === 0 ? "" : "border-t border-gray-200",
-                  "flex-1 py-10"
+          <div className="mt-4 flex items-center">
+            {[0, 1, 2, 3, 4].map((rating) => (
+              <StarIcon
+                key={rating}
+                className={clsx(
+                  rating < review.overallRating ? 'text-yellow-400' : 'text-gray-300',
+                  'h-5 w-5 flex-shrink-0'
                 )}
-              >
-                <h3 className="font-medium text-gray-900">{review.author}</h3>
-                <p>
-                  <time dateTime={review.datetime}>{review.date}</time>
-                </p>
+                aria-hidden="true"
+              />
+            ))}
+          </div>
+          <p className="sr-only">{review.overallRating} out of 5 stars</p>
 
-                <div className="mt-4 flex items-center">
-                  {[0, 1, 2, 3, 4].map((rating) => (
-                    <StarIcon
-                      key={rating}
-                      className={classNames(
-                        review.rating > rating
-                          ? "text-yellow-400"
-                          : "text-gray-300",
-                        "h-5 w-5 flex-shrink-0"
-                      )}
-                      aria-hidden="true"
-                    />
-                  ))}
-                </div>
-                <p className="sr-only">{review.rating} out of 5 stars</p>
-
-                <div
-                  className="prose prose-sm mt-4 max-w-none text-gray-500"
-                  dangerouslySetInnerHTML={{ __html: review.content }}
-                />
-              </div>
-            </div>
-          ))}
+          <div
+            className="prose prose-sm mt-4 max-w-none text-gray-500"
+            dangerouslySetInnerHTML={{ __html: review.comment }}
+          />
         </div>
       </div>
-    </div>
+    </li>
   );
-};
+}
+
 export default ReviewCard;
--- frontend/src/components/reviewsList/reviewsList.js
+++ frontend/src/components/reviewsList/reviewsList.js
@@ -2,9 +2,7 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import useAxios from "axios-hooks";
 import useAuthContext from "../../hooks/useAuthContext";
-import { StarIcon } from "@heroicons/react/20/solid";
-import clsx from "clsx";
-import { format } from 'date-fns';
+import ReviewCard from "../reviewCard/reviewCard";
 
 const propertyDetails = () => {
   const { id } = useParams();
@@ -23,38 +21,11 @@ const propertyDetails = () => {
   return (
     <ul role="list" className="space-y-3">
       {data.property.reviews.map((review) => (
-        <li key={review.id} className="overflow-hidden rounded-md bg-white px-6 py-4 shadow">
-          <div className="flex space-x-4 text-sm text-gray-500">
-            <div className="flex-1 py-10">
-              <h3 className="font-medium text-gray-900">{review.author.firstName}</h3>
-              <p>
-              <time dateTime={review.date}>{format(new Date(review.createdAt), 'dd, MMMM, yyyy')}</time>
-              </p>
-
-              <div className="mt-4 flex items-center">
-                {[0, 1, 2, 3, 4].map((rating) => (
-                  <StarIcon
-                    key={rating}
-                    className={clsx(
-                      rating < review.overallRating ? 'text-yellow-400' : 'text-gray-300',
-                      'h-5 w-5 flex-shrink-0'
-                    )}
-                    aria-hidden="true"
-                  />
-                ))}
-              </div>
-              <p className="sr-only">{review.overallRating} out of 5 stars</p>
-
-              <div
-                className="prose prose-sm mt-4 max-w-none text-gray-500"
-                dangerouslySetInnerHTML={{ __html: review.comment }}
-              />
-            </div>
-          </div>
-        </li>
+        <ReviewCard key={review.id} review={review} />
       ))}
     </ul>
   );
 };
 
 export default propertyDetails;
+
