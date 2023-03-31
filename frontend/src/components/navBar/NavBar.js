6c2aa269ea91a608a80710844ac339eef57d276f
mirandaweston <mweston105@yahoo.co.uk>
2023-03-31 20:49:38 +0100

    Added conditional: displays dropdown if user logged in and 'Log in' if not

--- frontend/src/components/navBar/NavBar.js
+++ frontend/src/components/navBar/NavBar.js
@@ -1,9 +1,5 @@
-// token
-// useAuthcontext
-// conditional if user logged in shows name & if not shows login button (token)
-
 import React, { Fragment, useEffect } from "react";
-import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { Disclosure, Menu, Transition, Combobox } from "@headlessui/react";
 import {
   MagnifyingGlassIcon,
   ChevronDownIcon,
@@ -92,63 +88,63 @@ const NavBar = () => {
                 </div>
 
                 {/* Profile dropdown */}
-                <Menu as="div" className="relative ml-4 shrink-0">
-                  <div>
-                    <Menu.Button className="-m-1.5 flex items-center p-1.5">
-                      <span className="sr-only">Open user menu</span>
-                      <span className="hidden lg:flex lg:items-center">
-                        <span
-                          className="ml-4 text-sm font-semibold leading-6 text-gray-900"
-                          aria-hidden="true"
-                        >
-                          {user && user.firstName}
+                {user ? (
+                  <Menu as="div" className="relative ml-4 shrink-0">
+                    <div>
+                      <Menu.Button className="-m-1.5 flex items-center p-1.5">
+                        <span className="sr-only">Open user menu</span>
+                        <span className="hidden lg:flex lg:items-center">
+                          <span
+                            className="ml-4 text-sm font-semibold leading-6 text-gray-900"
+                            aria-hidden="true"
+                          >
+                            {user.firstName}
+                          </span>
+                          <ChevronDownIcon
+                            className="text-black-600 ml-0 h-5 w-5"
+                            aria-hidden="true"
+                          />
                         </span>
-                        <ChevronDownIcon
-                          className="text-black-600 ml-0 h-5 w-5"
-                          aria-hidden="true"
-                        />
-                      </span>
-                    </Menu.Button>
-                  </div>
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
-                            )}
-                          >
-                            Your Profile
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
-                            )}
-                          >
-                            Settings
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <Link to="/" href="#">
+                      </Menu.Button>
+                    </div>
+                    <Transition
+                      as={Fragment}
+                      enter="transition ease-out duration-100"
+                      enterFrom="transform opacity-0 scale-95"
+                      enterTo="transform opacity-100 scale-100"
+                      leave="transition ease-in duration-75"
+                      leaveFrom="transform opacity-100 scale-100"
+                      leaveTo="transform opacity-0 scale-95"
+                    >
+                      <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                        <Menu.Item>
+                          {({ active }) => (
+                            <a
+                              href="#"
+                              className={classNames(
+                                active ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
+                              )}
+                            >
+                              Your Profile
+                            </a>
+                          )}
+                        </Menu.Item>
+                        <Menu.Item>
+                          {({ active }) => (
+                            <a
+                              href="#"
+                              className={classNames(
+                                active ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
+                              )}
+                            >
+                              Settings
+                            </a>
+                          )}
+                        </Menu.Item>
+                        <Menu.Item>
+                          {({ active }) => (
                             <button
                               type="button"
                               onClick={() => dispatch({ type: "logout" })}
@@ -159,12 +155,19 @@ const NavBar = () => {
                             >
                               Log out
                             </button>
-                          </Link>
-                        )}
-                      </Menu.Item>
-                    </Menu.Items>
-                  </Transition>
-                </Menu>
+                          )}
+                        </Menu.Item>
+                      </Menu.Items>
+                    </Transition>
+                  </Menu>
+                ) : (
+                  <Link
+                    to="/login"
+                    className="ml-4 text-sm font-semibold leading-6 text-gray-900"
+                  >
+                    Log in
+                  </Link>
+                )}
               </div>
             </div>
           </div>
