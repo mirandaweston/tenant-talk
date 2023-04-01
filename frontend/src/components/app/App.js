b51d790a5989f737d57ad42c05f253ec93386f2a
mirandaweston <mweston105@yahoo.co.uk>
2023-04-01 11:03:53 +0100

    Adding navbar to App.js

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -4,18 +4,22 @@ import Home from "../home/Home";
 import Signup from "../signup/Signup";
 import LoginForm from "../Login/loginForm";
 import Results from "../results/Results";
+import PropertyReviewsPage from "../propertyReviews/propertyReviews";
 import WithNav from "../withNav/WithNav";
+import AboutPage from "../aboutPage/aboutPage";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<LoginForm />} />
         <Route element={<WithNav />}>
+          <Route path="/" element={<Home />} />
           <Route path="/results" element={<Results />} />
         </Route>
+        <Route path="/property/:id" element={<PropertyReviewsPage />} />
+        <Route path="/about" element={<AboutPage />} />
       </Routes>
     </BrowserRouter>
   );
