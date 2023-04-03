2911df432070902eee23bf3ceac09cbf05b7daaa
Hayley Dobbs <iamdobbs@icloud.com>
2023-04-03 21:40:17 +0100

    Create Review.js and amend ReviewCard to navigate to reviewId page

--- frontend/src/components/reviewCard/ReviewCard.js
+++ frontend/src/components/reviewCard/ReviewCard.js
@@ -1,32 +1,36 @@
 import React from "react";
 import { format } from "date-fns";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 import Stars from "../stars/Stars";
 
 const ReviewCard = ({
-  review: { author, createdAt, overallRating, comment },
+  review: { _id, author, createdAt, overallRating, comment },
 }) => {
   return (
     <li className="overflow-hidden rounded-md bg-white px-6 py-4 shadow">
-      <div className="space-y-3 text-sm text-gray-500">
-        <div>
-          <h3 className="font-medium text-gray-900">{author.firstName}</h3>
-          <p>{format(new Date(createdAt), "dd MMMM yyyy")}</p>
-        </div>
+      <Link to={`/review/${_id}`}>
+        <div className="space-y-3 text-sm text-gray-500">
+          <div>
+            <h3 className="font-medium text-gray-900">{author.firstName}</h3>
+            <p>{format(new Date(createdAt), "dd MMMM yyyy")}</p>
+          </div>
 
-        <Stars rating={overallRating} />
-        <p className="sr-only">{overallRating} out of 5 stars</p>
+          <Stars rating={overallRating} />
+          <p className="sr-only">{overallRating} out of 5 stars</p>
 
-        <div className="prose prose-sm mt-4 max-w-none text-gray-900">
-          {comment}
+          <div className="prose prose-sm mt-4 max-w-none text-gray-900">
+            {comment}
+          </div>
         </div>
-      </div>
+      </Link>
     </li>
   );
 };
 
 ReviewCard.propTypes = {
   review: PropTypes.shape({
+    _id: PropTypes.string,
     author: PropTypes.shape({
       firstName: PropTypes.string,
     }),
