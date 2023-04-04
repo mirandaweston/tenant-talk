001c3b5b7feb03d6e1377560fe709a0c4e279e04
Hayley Dobbs <iamdobbs@icloud.com>
2023-04-04 22:41:46 +0100

    Changes to AboutPage

--- frontend/src/components/aboutPage/aboutPage.js
+++ frontend/src/components/aboutPage/aboutPage.js
@@ -37,6 +37,12 @@ const team = [
     imageUrl:
       "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
   },
+  {
+    name: "Joel Powell",
+    role: "Senior Dev",
+    imageUrl: "/frontend/public/assets/joel.png",
+  },
+
   // More people...
 ];
 
