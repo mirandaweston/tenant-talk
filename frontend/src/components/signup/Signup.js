f19ca967fba76ed15ca101cd42c7f6ef7159acb4
Joel Powell <joelhpowell@icloud.com>
2023-04-05 12:18:47 +0100

    refactored login and signup pages, added form validation and toasts

--- frontend/src/components/signup/Signup.js
+++ frontend/src/components/signup/Signup.js
@@ -1,145 +1,155 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
+import clsx from "clsx";
 import useSignup from "../../hooks/useSignup";
-import useAuthContext from "../../hooks/useAuthContext";
+
+const fields = [
+  {
+    name: "firstName",
+    label: "First name",
+    type: "text",
+    autoComplete: "given-name",
+  },
+  {
+    name: "lastName",
+    label: "Last name",
+    type: "text",
+    autoComplete: "family-name",
+  },
+  {
+    name: "email",
+    label: "Email",
+    type: "text",
+    autoComplete: "email",
+    style: "col-span-full",
+  },
+  {
+    name: "password",
+    label: "Password",
+    type: "password",
+    autoComplete: "new-password",
+  },
+  {
+    name: "confirmPassword",
+    label: "Confirm Password",
+    type: "password",
+    autoComplete: "new-password",
+  },
+];
 
 const Signup = () => {
-  const { signup, error, isLoading } = useSignup();
-  const { register, handleSubmit, reset } = useForm();
-  const { user } = useAuthContext();
+  const { signup, isLoading } = useSignup();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    watch,
+    formState: { errors },
+  } = useForm();
 
   useEffect(() => {
     if (isLoading) return;
-    reset();
+    reset({ password: null, confirmPassword: null });
   }, [isLoading]);
 
   return (
-    <div className="relative flex h-screen justify-center md:px-12 lg:px-0">
-      <div className="relative z-10 flex flex-1 flex-col bg-white  sm:justify-center md:flex-none md:px-28">
-        <div className="mx-auto w-full max-w-md sm:px-4 md:w-96 md:max-w-sm md:px-0">
-          <div className="flex flex-col">
-            <div className="mt-20">
-              {user && <p>{`Hello ${user.firstName}!`}</p>}
-              <h2 className="text-lg font-semibold text-gray-900">
-                Sign up for an account
-              </h2>
-              <p className="mt-2 text-sm text-gray-700">
-                Already registered?{" "}
-                <Link
-                  to="/login"
-                  className="font-medium text-blue-600 hover:underline"
-                >
-                  Log in
-                </Link>{" "}
-                to your account.
+    <>
+      <svg
+        className="h-12 w-auto fill-orange-500"
+        viewBox="0 0 20 20"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M1,0 h18 a1,1 0 0 1 1,1 v6 a1,1 0 0 1 -1,1 h-5 l-8 8 v3 a1,1 0 0 0 1,1 h6 a1,1 0 0 0 1,-1 v-3 l-8 -8 h-5 a1,1 0 0 1 -1,-1 v-6 a1,1 0 0 1 1,-1 z" />
+      </svg>
+
+      <h2 className="mt-6 text-3xl font-bold tracking-tight text-gray-900">
+        Sign up for an account
+      </h2>
+
+      <p className="mt-2 text-sm text-gray-600">
+        Already registered?{" "}
+        <Link
+          to="/login"
+          className="font-medium text-orange-500 hover:text-orange-400"
+        >
+          Log in
+        </Link>{" "}
+        to your account.
+      </p>
+
+      <form
+        onSubmit={handleSubmit(signup)}
+        className="mt-8 grid grid-cols-1 gap-6 sm:grid-cols-2"
+      >
+        {fields.map(({ name, label, type, autoComplete, style }) => (
+          <div key={name} className={style}>
+            <label
+              htmlFor={name}
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              {label}
+            </label>
+            <div className="mt-2">
+              <input
+                id={name}
+                type={type}
+                autoComplete={autoComplete}
+                className={clsx(
+                  errors[name]
+                    ? "ring-red-500 focus:ring-red-500"
+                    : "ring-gray-300 focus:ring-orange-500",
+                  "block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset placeholder:text-gray-400 focus:ring-2 focus:ring-inset  sm:text-sm sm:leading-6"
+                )}
+                {...register(name, {
+                  required: `${label} is required`,
+                  ...(name === "email" && {
+                    pattern: {
+                      value:
+                        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                      message: "Invalid email address",
+                    },
+                  }),
+                  ...(name === "password" && {
+                    pattern: {
+                      value: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$/,
+                      message:
+                        "Password must contain at least eight characters, at least one uppercase letter, one lowercase letter and one number",
+                    },
+                  }),
+                  ...(name === "confirmPassword" && {
+                    validate: (value) => {
+                      if (watch("password") !== value)
+                        return "Passwords do not match";
+                    },
+                  }),
+                })}
+                data-cy={name}
+              />
+            </div>
+            {errors[name] && (
+              <p
+                role="alert"
+                className="ml-1 mt-1 text-xs font-medium tracking-wide text-red-500"
+              >
+                {errors[name].message}
               </p>
-            </div>
-          </div>
-          <form
-            className="mt-10 grid grid-cols-1 gap-y-8 gap-x-6 sm:grid-cols-2"
-            onSubmit={handleSubmit(signup)}
-          >
-            <div>
-              <label
-                id="first_name"
-                htmlFor="first_name"
-                className="mb-3 block text-sm font-medium text-gray-700"
-              >
-                First name
-              </label>
-              <input
-                data-cy="first_name"
-                id="first_name"
-                name="first_name"
-                type="text"
-                autoComplete="given-name"
-                className="block w-full appearance-none rounded-md border border-gray-200 bg-gray-50 px-3 py-2 text-gray-900 placeholder:text-gray-400 focus:border-blue-500 focus:bg-white focus:outline-none focus:ring-blue-500 sm:text-sm"
-                {...register("firstName", { required: true, maxLength: 20 })}
-              />
-            </div>
-            <div>
-              <label
-                id="last_name"
-                htmlFor="last_name"
-                className="mb-3 block text-sm font-medium text-gray-700"
-              >
-                Last name
-              </label>
-              <input
-                data-cy="last_name"
-                id="last_name"
-                name="last_name"
-                type="text"
-                autoComplete="family-name"
-                className="block w-full appearance-none rounded-md border border-gray-200 bg-gray-50 px-3 py-2 text-gray-900 placeholder:text-gray-400 focus:border-blue-500 focus:bg-white focus:outline-none focus:ring-blue-500 sm:text-sm"
-                {...register("lastName", { required: true, maxLength: 20 })}
-              />
-            </div>
-            <div className="col-span-full">
-              <label
-                id="email"
-                htmlFor="email"
-                className="mb-3 block text-sm font-medium text-gray-700"
-              >
-                Email
-              </label>
-              <input
-                data-cy="email"
-                id="email"
-                name="email"
-                type="email"
-                autoComplete="email"
-                className=" block w-full appearance-none rounded-md border border-gray-200 bg-gray-50 px-3 py-2 text-gray-900 placeholder:text-gray-400 focus:border-blue-500 focus:bg-white focus:outline-none focus:ring-blue-500 sm:text-sm"
-                {...register("email", { required: true })}
-              />
-            </div>
-            <div className="col-span-full">
-              <label
-                id="password"
-                htmlFor="password"
-                className="mb-3 block text-sm font-medium text-gray-700"
-              >
-                Password
-              </label>
-              <input
-                data-cy="password"
-                id="password"
-                name="password"
-                type="password"
-                autoComplete="new-password"
-                className="block w-full appearance-none rounded-md border border-gray-200 bg-gray-50 px-3 py-2 text-gray-900 placeholder:text-gray-400 focus:border-blue-500 focus:bg-white focus:outline-none focus:ring-blue-500 sm:text-sm"
-                {...register("password", { required: true })}
-              />
-            </div>
-            {error && (
-              <div className="col-span-full">
-                <p data-cy="error" className="text-red-500">
-                  {error}
-                </p>
-              </div>
             )}
-            <div className="col-span-full">
-              <button
-                data-cy="submit"
-                type="submit"
-                disabled={isLoading}
-                className="flex w-full justify-center rounded-md bg-orange-500 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-orange-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-500 disabled:cursor-not-allowed disabled:bg-gray-100 disabled:text-gray-500"
-              >
-                Sign in
-              </button>
-            </div>
-          </form>
+          </div>
+        ))}
+
+        <div className="col-span-full">
+          <button
+            type="submit"
+            disabled={isLoading}
+            className="flex w-full justify-center rounded-md bg-orange-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-orange-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-500 disabled:cursor-not-allowed disabled:bg-gray-100 disabled:text-gray-500"
+            data-cy="submit"
+          >
+            Sign up
+          </button>
         </div>
-      </div>
-      <div className="hidden sm:contents lg:relative lg:block lg:flex-1">
-        <img
-          className="absolute inset-0 h-full w-full object-cover"
-          src="https://source.unsplash.com/h95mT1m9Zzs/5034x3696"
-          alt=""
-        />
-      </div>
-    </div>
+      </form>
+    </>
   );
 };
 
